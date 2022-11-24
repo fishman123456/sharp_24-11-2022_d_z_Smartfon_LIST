c03298bf708087bc34862cd06a636ba9744c6c09
denis <106389581+fishman123456@users.noreply.github.com>
2022-11-24 14:25:05 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿class smartfon
+{
+    public string? _name { get; set; }
+    public int? _year { get; set; }
+    public string? _color { get; set; }
+    public string? _prise { get; set; }
+
+
+}
